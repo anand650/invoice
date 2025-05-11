cc078c085438de4c18a267fcb5b7fac531adca10
Anand <anand.jadhav19@vit.edu>
2025-05-11 12:23:55 +0530

    ADDED compare api

--- api/integration/index.js
+++ api/integration/index.js
@@ -8,11 +8,18 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const N8N_SECRET = 'e2b74c5d-9140-4a4c-90f0-f64da4b3fd42';
+
+// Actual destination API info
+const DESTINATION_URL = 'https://cargomation.com:5200/redis/apinvoice/compare';
+const AUTH_USERNAME = 'admin';
+const AUTH_PASSWORD = 'u}M[6zzAU@w8YLx';
+
 app.post('/invoice', async (req, res) => {
   try {
-    const n8nWebhookUrl = 'https://n8n.srv788488.hstgr.cloud/webhook/2a6287d0-9458-4421-8a13-56c0216f730f';
+    const webhookUrl = 'https://n8n.srv788488.hstgr.cloud/webhook/2a6287d0-9458-4421-8a13-56c0216f730f';
 
-    const response = await axios.post(n8nWebhookUrl, req.body);
+    const response = await axios.post(webhookUrl, req.body);
 
     res.status(response.status).json(response.data);
   } catch (err) {
@@ -20,6 +27,40 @@ app.post('/invoice', async (req, res) => {
     res.status(500).json({ error: 'Forwarding to n8n failed' });
   }
 });
+
+
+app.post('/compare', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || authHeader !== `Bearer ${N8N_SECRET}`) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const response = await axios.post(
+      DESTINATION_URL,
+      req.body,
+      {
+        auth: {
+          username: AUTH_USERNAME,
+          password: AUTH_PASSWORD
+        },
+        headers: {
+          'User-Agent': 'axios/0.27.2',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Connection': 'keep-alive'
+        }
+      }
+    );
+
+    return res.status(response.status).json(response.data);
+  } catch (err) {
+    console.error('Error forwarding request:', err.response?.data || err.message);
+    return res.status(500).json({ message: 'Failed to forward request', error : err });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
